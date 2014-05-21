896e96030e3b4ad446e56726271cad6138d02674
rahular <rahulife007@gmail.com>
2014-05-22 00:16:01 +0530

    doc init

--- src/com/rahul/phoenix/tui/TestTournament.java
+++ src/com/rahul/phoenix/tui/TestTournament.java
@@ -20,14 +20,26 @@ public class TestTournament {
 	}
 
 	private static void play(double[] genes) {
-		int numOfMatches = 250;
+		int numOfMatches = 2;
 		double whiteScore = 0;
 		long start = System.currentTimeMillis();
 
+		String header = "[Event \"Training Tournament\"]\n"
+				+ "[White \"mgGenes4\"]\n" + "[Black \"defaultGenes\"]\n"
+				+ "[Date \"2014.05.04\"]\n" + "[TimeControl \"5|0\"]\n"
+				+ "[Result \"";
+		String tail = "\"]\n";
+
 		for (int i = 0; i < numOfMatches; i++) {
+			System.out.print(header);
+			String result = TestMatch.play(genes);
+			System.out.print(result);
+			System.out.println(tail);
+			
 			whiteScore += Double
-					.parseDouble(TestMatch.play(genes).split("-")[0]);
+					.parseDouble(result.split("-")[0]);
 			System.out.println(TestMatch.pgn);
+			System.out.println();
 		}
 
 		long end = System.currentTimeMillis();
